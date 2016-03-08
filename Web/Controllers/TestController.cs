261103b1dd07b9092a6e2538bc2e4a944c3b0ef3
dresser <matthew.dresser@valtech.com>
2016-03-08 11:34:51 +0100

    Updated to include detailed rendering information on TestRendering.cshtml.

--- Web/Controllers/TestController.cs
+++ Web/Controllers/TestController.cs
@@ -13,20 +13,37 @@ namespace Web.Controllers
 
         public ActionResult ContextItemRendering()
         {
-            var model = new TestModel(RenderingContext.Current.ContextItem);
-            return View(model);
+            var model = new TestModel(RenderingContext.Current.ContextItem,
+                "RenderingContext.Current.ContextItem",
+                GetDataSourcePath(RenderingContext.Current));
+            return View("TestRendering", model);
         }
 
         public ActionResult PageContextRendering()
         {
-            var model = new TestModel(RenderingContext.Current.PageContext.Item);
-            return View(model);
+            var model = new TestModel(RenderingContext.Current.PageContext.Item,
+                "RenderingContext.Current.PageContext.Item", 
+                GetDataSourcePath(RenderingContext.Current));
+            return View("TestRendering", model);
         }
 
         public ActionResult RenderingItemRendering()
         {
-            var model = new TestModel(RenderingContext.Current.Rendering.Item);
-            return View(model);
+            var model = new TestModel(RenderingContext.Current.Rendering.Item,
+                "RenderingContext.Current.Rendering.Item",
+                GetDataSourcePath(RenderingContext.Current));
+            return View("TestRendering", model);
+        }
+
+        private string GetDataSourcePath(RenderingContext renderingContext)
+        {
+            var datasourceItem =
+                renderingContext.PageContext.Database.GetItem(renderingContext.Rendering.DataSource);
+            if (datasourceItem == null)
+            {
+                return "";
+            }
+            return datasourceItem.Paths.ContentPath;
         }
     }
 }
--- Web/Models/TestModel.cs
+++ Web/Models/TestModel.cs
@@ -4,11 +4,20 @@ namespace Web.Models
 {
     public class TestModel
     {
-        public TestModel(Sitecore.Data.Items.Item item)
+        public TestModel(Sitecore.Data.Items.Item item, string dataSourceProperty, string currentRenderingDataSource)
         {
             Text = new HtmlString(Sitecore.Web.UI.WebControls.FieldRenderer.Render(item, "Text"));
+            Path = item.Paths.ContentPath;
+            DataSourceProperty = dataSourceProperty;
+            CurrentRenderingDataSource = currentRenderingDataSource;
         }
 
         public IHtmlString Text { get; private set; }
+
+        public string Path { get; private set; }
+
+        public string DataSourceProperty { get; private set; }
+
+        public string CurrentRenderingDataSource { get; private set; }
     }
 }
