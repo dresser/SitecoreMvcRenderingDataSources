b23acfe790d646ec1b1b55c7f351f1bc5f0fa824
dresser <matthew.dresser@valtech.com>
2016-03-08 02:03:04 +0100

    Added test renderings to demonstrate differences between RenderingContext.Current.ContextItem, RenderingContext.Current.PageContext.Item and RenderingContext.Current.Rendering.Item.

---
+++ Web/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Sitecore.Mvc.Presentation;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Container()
+        {
+            return View();
+        }
+
+        public ActionResult ContextItemRendering()
+        {
+            var model = new TestModel(RenderingContext.Current.ContextItem);
+            return View(model);
+        }
+
+        public ActionResult PageContextRendering()
+        {
+            var model = new TestModel(RenderingContext.Current.PageContext.Item);
+            return View(model);
+        }
+
+        public ActionResult RenderingItemRendering()
+        {
+            var model = new TestModel(RenderingContext.Current.Rendering.Item);
+            return View(model);
+        }
+    }
+}
---
+++ Web/Models/TestModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+
+namespace Web.Models
+{
+    public class TestModel
+    {
+        public TestModel(Sitecore.Data.Items.Item item)
+        {
+            Text = new HtmlString(Sitecore.Web.UI.WebControls.FieldRenderer.Render(item, "Text"));
+        }
+
+        public IHtmlString Text { get; private set; }
+    }
+}
